6dda32ec5966446dbde8a94c716589c78b3e28df
Kevin Lie <kevinyilie@gmail.com>
2023-07-05 11:40:07 -0400

    Add account class and controllers

---
+++ capstone/TenmoServer/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TenmoServer.DAO;
+using TenmoServer.Exceptions;
+using TenmoServer.Models;
+using TenmoServer.Security;
+
+namespace TenmoServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public IAccountDao accountDao;
+        public AccountController(IAccountDao accountDao)
+        {
+            this.accountDao = accountDao;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<decimal> GetBalance(int id)
+        {
+            return Ok(accountDao.GetBalance(id));
+        }
+    }
+}
---
+++ capstone/TenmoServer/DAO/AccountSqlDao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using TenmoServer.Exceptions;
+using TenmoServer.Models;
+using TenmoServer.Security;
+using TenmoServer.Security.Models;
+
+namespace TenmoServer.DAO
+{
+    public class AccountSqlDao : IAccountDao
+    {
+        private readonly string connectionString;
+        const decimal StartingBalance = 1000M;
+
+        public AccountSqlDao(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public decimal GetBalance(int userId)
+        {
+            decimal balance = 0;
+            string sql = "SELECT balance FROM account WHERE user_id = @user_id ";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@user_id", userId);
+                    balance = Convert.ToDecimal(cmd.ExecuteScalar());
+                }
+            }
+            return balance;
+        }
+
+        private Account MapRowToAccount(SqlDataReader reader)
+        {
+            Account account = new Account();
+            account.AccountId = Convert.ToInt32(reader["account_id"]);
+            account.UserId = Convert.ToInt32(reader["user_id"]);
+            account.Balance = Convert.ToDecimal(reader["user_id"]);
+
+            return account;
+        }
+    }
+}
